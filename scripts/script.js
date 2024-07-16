152819ccad9e52c52311046fa4780d9a602cf2c2
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-07-16 10:07:59 +0200

    🚧 masonry grid for search result display

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,29 @@
+// import Masonry from 'masonry-layout'
+
+// window.onload = () => {
+//   const masonryGrid = document.querySelector('.masonry-grid')
+//   console.log(masonryGrid)
+//   const masonry = new Masonry(grid)
+// }
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('script loaded')
 
   //Getting input
-  const inputSearch = document.querySelector('.input-search')
-  let userInput = ''
-  inputSearch.addEventListener('input', (e) => (userInput = e.target.value))
+  // const inputSearch = document.querySelector('.input-search')
+  // if (inputSearch) {
+  //   let userInput = ''
+  //   inputSearch.addEventListener('input', (e) => (userInput = e.target.value))
+  // }
+
+  //Getting search-result-wrapper
+  const searchResultWrapper = document.querySelector('.search-result-wrapper')
+  if (searchResultWrapper) {
+    console.log(searchResultWrapper)
+    const height = searchResultWrapper.offsetHeight
+    console.log(height)
+    console.log(searchResultWrapper)
+    searchResultWrapper.style.height = height / 5
+    console.log(searchResultWrapper)
+  }
 })
