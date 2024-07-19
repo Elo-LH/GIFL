ec15baa90813532a27e7f90b079c1daccab85028
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-07-19 10:44:57 +0200

    🔒️ password complexity required

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends AbstractController
                 $userFound = $instance->findByEmail($email);
                 //if user not found in db give error
                 if (!$userFound) {
-                    $this->redirect("index.php?route=error&error=Unknown Email, please register first.");
+                    $this->redirect("index.php?route=error&error=Unknown Email $email, please register first.");
                     //if user found check password
                 } else {
                     $hashFound = $userFound->getPassword();
@@ -65,36 +65,48 @@ class AuthController extends AbstractController
 
         // check if data received from form
         if (isset($_POST["email"]) && isset($_POST["name"]) && isset($_POST["password"]) && isset($_POST["password-confirm"])) {
+
             // check CSRF token
             $tokenManager = new CSRFTokenManager();
-
             if (isset($_POST["csrf-token"]) && $tokenManager->validateCSRFToken($_POST["csrf-token"])) {
 
-                //get form data
-                $email = htmlspecialchars($_POST['email']);
-                $name = htmlspecialchars($_POST['name']);
-                $password = htmlspecialchars($_POST['password']);
-                $passwordConfirm = htmlspecialchars($_POST['password-confirm']);
-                $avatar = htmlspecialchars($_POST['avatar']);
-                // If no avatar chosen byc user, assign one random GIF
-                if ($avatar == "") {
-                    $gm = new GifManager;
-                    $avatar = $gm->getRandom1()->getLink();
-                }
-                $hash = password_hash($password, PASSWORD_DEFAULT);
-                //init manager
-                $user = new User($email, $name, $hash, $avatar);
-                $instance = new UserManager;
-                //find user 
-                $userFound = $instance->findByEmail($email);
-                //if email allready used give error
-                if ($userFound) {
-                    $this->redirect("index.php?route=error&error=Email allready used, please log in.");
-                    //if email not found create new user
+                //check confirm password 
+                if ($_POST["password"] === $_POST["password-confirm"]) {
+                    //check password is 8chars minimum with at least 1 maj, 1min 1 number and 1 special char
+                    $password_pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,}/';
+                    if (preg_match($password_pattern, $_POST["password"])) {
+
+                        //get form data
+                        $email = htmlspecialchars($_POST['email']);
+                        $name = htmlspecialchars($_POST['name']);
+                        $avatar = htmlspecialchars($_POST['avatar']);
+                        // If no avatar chosen byc user, assign one random GIF
+                        if ($avatar == "") {
+                            $gm = new GifManager;
+                            $avatar = $gm->getRandom1()->getLink();
+                        }
+                        $hash = password_hash($_POST["password"], PASSWORD_BCRYPT);
+                        //init manager
+                        $user = new User($email, $name, $hash, $avatar);
+                        $instance = new UserManager;
+                        //find user 
+                        $userFound = $instance->findByEmail($email);
+                        //if email allready used give error
+                        if ($userFound) {
+                            $this->redirect("index.php?route=error&error=Email allready used, please log in.");
+                            //if email not found create new user
+                        } else {
+                            $instance->createUser($user);
+                            //redirect to home
+                            $this->redirect("index.php?route=home");
+                        }
+                    } else {
+                        $this->redirect("index.php?route=error&error=Passwords must be at least 8 characters long, with at least one uppercase letter, one lowercase letter, one number and one special character.
+
+");
+                    }
                 } else {
-                    $instance->createUser($user);
-                    //redirect to home
-                    $this->redirect("index.php?route=home");
+                    $this->redirect("index.php?route=error&error=Passwords do not match");
                 }
             } else {
                 $this->redirect("index.php?route=error&error=Invalid CSRF token");
