ab883ff0852de80f6f0536d2f4d80790fa217eb6
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-07-23 12:03:49 +0200

    🔒️ admin paths authorized only to signed in admins

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -5,7 +5,7 @@ class AdminController extends AbstractController
 
     public function backOffice(): void
     {
-        if (isset($_SESSION['email'])) {
+        if (isset($_SESSION['admin'])) {
             //get all users
             $um = new UserManager();
             $users = $um->findAll();
@@ -16,59 +16,75 @@ class AdminController extends AbstractController
     }
     public function deleteUser(): void
     {
-        //get user id from params
-        if (isset($_GET['user'])) {
-            $id = $_GET['user'];
-            //init manager
-            $um = new UserManager();
-            $um->deleteUser($id);
-            $this->redirect("index.php?route=back-office");
+        if (isset($_SESSION['admin'])) {
+            //get user id from params
+            if (isset($_GET['user'])) {
+                $id = $_GET['user'];
+                //init manager
+                $um = new UserManager();
+                $um->deleteUser($id);
+                $this->redirect("index.php?route=back-office");
+            }
+        } else {
+            $this->redirect("index.php?route=home");
         }
     }
     public function updateUser(): void
     {
-        if (isset($_GET['user'])) {
-            $id = $_GET['user'];
-            //init manager
-            $um = new UserManager;
-            //if modifications have been submitted
-            if (isset($_POST['email']) && isset($_POST['name'])) {
-                if (isset($_POST['avatar']) && $_POST['avatar'] == "") {
-                    $gm = new GifManager;
-                    $avatar = $gm->getRandom1()->getLink();
-                } else {
-                    $avatar = $_POST['avatar'];
+        if (isset($_SESSION['admin'])) {
+            if (isset($_GET['user'])) {
+                $id = $_GET['user'];
+                //init manager
+                $um = new UserManager;
+                //if modifications have been submitted
+                if (isset($_POST['email']) && isset($_POST['name'])) {
+                    if (isset($_POST['avatar']) && $_POST['avatar'] == "") {
+                        $gm = new GifManager;
+                        $avatar = $gm->getRandom1()->getLink();
+                    } else {
+                        $avatar = $_POST['avatar'];
+                    }
+                    //apply modifications in db
+                    $um->updateUser($id, $_POST['email'], $_POST['name'], $avatar);
                 }
-                //apply modifications in db
-                $um->updateUser($id, $_POST['email'], $_POST['name'], $avatar);
+                //find user
+                $user = $um->findById($id);
+                $user->setId($id);
+                //get all users
+                $users = $um->findAll();
+                $this->render("back-office.html.twig", ["users" => $users, "user" => $user]);
             }
-            //find user
-            $user = $um->findById($id);
-            $user->setId($id);
-            //get all users
-            $users = $um->findAll();
-            $this->render("back-office.html.twig", ["users" => $users, "user" => $user]);
+        } else {
+            $this->redirect("index.php?route=home");
         }
     }
     public function createUser(): void
     {
-        //init manager
-        $instance = new UserManager;
-        //get post info
-        $user = new User($_POST['name'], $_POST['email'], $_POST['password'], $_POST['role']);
-        $post = $instance->createUser($user);
+        if (isset($_SESSION['admin'])) {
+            //init manager
+            $instance = new UserManager;
+            //get post info
+            $user = new User($_POST['name'], $_POST['email'], $_POST['password'], $_POST['role']);
+            $post = $instance->createUser($user);
 
-        header('Location:index.php?route=adminpage');
+            header('Location:index.php?route=adminpage');
+        } else {
+            $this->redirect("index.php?route=home");
+        }
     }
     public function toggleAdmin(): void
     {
-        if (isset($_GET['user'])) {
-            $id = $_GET['user'];
-            //init manager
-            $um = new UserManager;
-            //get post info
-            $um->toggleAdmin($id);
-            $this->redirect("index.php?route=back-office");
+        if (isset($_SESSION['admin'])) {
+            if (isset($_GET['user'])) {
+                $id = $_GET['user'];
+                //init manager
+                $um = new UserManager;
+                //get post info
+                $um->toggleAdmin($id);
+                $this->redirect("index.php?route=back-office");
+            }
+        } else {
+            $this->redirect("index.php?route=home");
         }
     }
 }
