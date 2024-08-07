74eb53a065ed8f0839bb2e62924e2c923d1ea01e
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-08-07 14:39:48 +0200

    🚧 gif modale for search results

--- scripts/script-authentified.js
+++ scripts/script-authentified.js
@@ -83,6 +83,10 @@ const getParameter = (key) => {
   return parameterList.get(key)
 }
 
+/* ==========================================================================
+DOM CONTENT LOADED
+========================================================================== */
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('script loaded')
 
@@ -119,17 +123,25 @@ document.addEventListener('DOMContentLoaded', () => {
               console.log(data)
               data.forEach((gif) => {
                 const collectionId = getParameter('collection')
+                //masonry-grid-item
                 const gridItem = document.createElement('div')
                 gridItem.classList.add('masonry-grid-item')
-                const gridLink = document.createElement('a')
-                gridLink.classList.add('masonry-grid-link')
-                gridLink.href = `index.php?route=gif&gif=${gif.id}`
+                //masonry span for gif modale
+                const spanGIF = document.createElement('span')
+                spanGIF.classList.add('masonry-grid-link', 'js-gif-modale')
+                spanGIF.id = `js-gif-id-${gif.id}`
+                spanGIF.position = 'relative'
+                //masonry gif img
                 const gifImg = document.createElement('img')
                 gifImg.classList.add('masonry-grid-img')
                 gifImg.alt = ''
                 gifImg.src = gif.link
-                gridLink.appendChild(gifImg)
-                gridItem.appendChild(gridLink)
+                gifImg.id = `js-gif-id-${gif.id}`
+
+                //appending all elements
+                spanGIF.appendChild(gifImg)
+                gridItem.appendChild(spanGIF)
+
                 if (
                   searchResultDisplay.classList.contains('js-collection-add')
                 ) {
@@ -142,9 +154,172 @@ document.addEventListener('DOMContentLoaded', () => {
                   )
                   gridAddLink.href = `index.php?route=add-gif-to-collection&gif=${gif.id}&collection=${collectionId}`
                   gridAddLink.textContent = 'Add'
-                  gridItem.appendChild(gridAddLink)
+                  spanGIF.appendChild(gridAddLink)
                 }
                 searchResultDisplay.appendChild(gridItem)
+
+                if (gifImg.complete) {
+                  //load masonry display
+                  var elem = document.querySelector('.masonry-grid')
+                  var msnry = new Masonry(elem, {
+                    // options
+                    itemSelector: '.masonry-grid-item',
+
+                    gutter: 2,
+                  })
+                }
+                // Add event listener on each gif item
+                const gifModale = document.querySelector('.gif-display-modale')
+                const gifItems = document.querySelectorAll('.js-gif-modale')
+                const authOptions = document.querySelector(
+                  '.gif-card-auth-options'
+                )
+                gridItem.addEventListener('click', (e) => {
+                  console.log('event click modale')
+                  // If click on GIF
+
+                  //retrieve GIF id
+                  const gifIdName = e.target.id
+                  const gifId = gifIdName.slice(10)
+                  console.log(gifId)
+
+                  // generate report button
+                  authOptions.innerHTML = ''
+                  authOptions.classList.toggle('modale-hidden')
+                  const reportBtn = document.createElement('button')
+                  reportBtn.classList.add(
+                    'pink-button',
+                    'very-small-button',
+                    'gif-report-btn'
+                  )
+                  reportBtn.textContent = 'Report'
+                  reportBtn.addEventListener('click', () => {
+                    fetch(api + 'put-gif-reported&gif=' + gifId).then(
+                      (response) => {
+                        console.log(response)
+                        if (response.ok) {
+                          //response.json().then(console.log)
+                          response.json().then((data) => {
+                            console.log('La requête a  réussi')
+                            console.log(data)
+                            return data
+                          })
+                        } else {
+                          // La requete a echoué
+                          console.log('La requête a échoué')
+                          console.log(response)
+                        }
+                      }
+                    )
+                  })
+
+                  authOptions.appendChild(reportBtn)
+
+                  //Generate select
+                  const selectCollection = document.createElement('select')
+                  //fetch collections from user
+                  //for each collection generate an option with value = collection_id and collection name
+
+                  // Fetch gif infos from API
+                  fetch(api + 'get-gif-info&gif=' + gifId).then((response) => {
+                    console.log(response)
+                    if (response.ok) {
+                      //response.json().then(console.log)
+                      response.json().then((data) => {
+                        console.log('La requête a  réussi')
+                        console.log(data)
+                        // use fetched data to generate modale
+
+                        // add gif img
+                        const gifImg =
+                          document.querySelector('.js-gif-modale-img')
+                        gifImg.src = data[0].link
+                        const gifHashtags = document.querySelector(
+                          '.js-gif-modale-hashtags'
+                        )
+                        // add each hashtag link
+                        data.forEach((hashtag, i) => {
+                          if (i == 0) {
+                            gifHashtags.innerHTML = ''
+                          } else {
+                            const gifHashtag = document.createElement('a')
+                            gifHashtag.textContent = `#${hashtag.name}`
+                            gifHashtag.classList.add('gif-card-hashtag')
+                            gifHashtag.href = `index.php?route=hashtag-page&hashtag=${hashtag.id}`
+                            gifHashtags.appendChild(gifHashtag)
+                          }
+                        })
+                        // fill input with link to gif page
+                        const input = document.querySelector(
+                          '.js-gif-modale-input'
+                        )
+                        input.value = data[0].link
+                        //show modale
+                        gifModaleOverlay.classList.toggle('modale-hidden')
+                        gifModale.classList.toggle('modale-hidden')
+
+                        // Getting copy link button for gif modale
+                        const copyBtnGif =
+                          document.querySelector('.js-copy-btn-gif')
+                        if (copyBtnGif) {
+                          console.log('add copy adress event for gif')
+                          copyBtnGif.addEventListener('click', copyAdressGif)
+                        }
+
+                        //getting download button for gif modale
+                        const downloadBtn =
+                          document.querySelector('.js-download-gif')
+                        if (downloadBtn) {
+                          console.log('add event listener to dwnld btn')
+                          downloadBtn.addEventListener(
+                            'click',
+                            downloadGif,
+                            false
+                          )
+
+                          async function downloadGif() {
+                            console.log('enter download gif')
+                            //create new a element
+                            let a = document.createElement('a')
+                            // get image as blob
+                            let response = await fetch(data[0].link)
+                            let file = await response.blob()
+                            console.log(file)
+                            // use download attribute https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Attributes
+                            a.download = data[0].id
+                            a.href = window.URL.createObjectURL(file)
+                            console.log(a.href)
+                            //store download url in javascript https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes#JavaScript_access
+                            a.dataset.downloadurl = [
+                              'application/octet-stream',
+                              a.download,
+                              a.href,
+                            ].join(':')
+                            //click on element to start download
+                            a.click()
+                            window.URL.revokeObjectURL(file)
+                            downloadBtn.removeEventListener(
+                              'click',
+                              downloadGif,
+                              false
+                            )
+                            console.log('remove event listener to dwnld btn')
+                          }
+                        }
+                      })
+                    } else {
+                      // La requete a echoué
+                      console.log('La requête a échoué')
+                      console.log(response)
+                    }
+                  })
+                })
+                //Add keyboard control to open modale (accessibility)
+                gridItem.addEventListener('keydown', (event) => {
+                  if (event.code === 'Space' || event.code === 'Enter') {
+                    gifItem.click()
+                  }
+                })
               })
             })
           } else {
@@ -370,12 +545,17 @@ document.addEventListener('DOMContentLoaded', () => {
       fileInput.files = e.dataTransfer.files
     })
   }
-  //load masonry display
-  var elem = document.querySelector('.masonry-grid')
-  var msnry = new Masonry(elem, {
-    // options
-    itemSelector: '.masonry-grid-item',
+  window.onload = () => {
+    // const masonryGrid = document.querySelector('.masonry-grid')
+    // console.log(masonryGrid)
+    // const masonry = new Masonry(grid)
 
-    gutter: 5,
-  })
+    var elem = document.querySelector('.masonry-grid')
+    var msnry = new Masonry(elem, {
+      // options
+      itemSelector: '.masonry-grid-item',
+
+      gutter: 2,
+    })
+  }
 })
--- scripts/script.js
+++ scripts/script.js
@@ -70,6 +70,31 @@ function copyAdress() {
     copyMessage.style.display = 'none'
   }, 2000)
 }
+function copyAdressGif() {
+  console.log('copyAdressGif')
+  // Copy the text inside the text field
+  const inputSearch = document.querySelector('.js-input-search-gif')
+  const link = inputSearch.value
+  console.log(link)
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(link)
+  } else {
+    const input = document.createElement('textarea')
+    input.value = link
+    document.body.appendChild(input)
+    input.select()
+    document.execCommand('copy')
+    document.body.removeChild(input)
+  }
+  const copyMessage = document.querySelector('.js-copy-message-gif')
+
+  // Alert the copied text
+  console.log(copyMessage)
+  copyMessage.style.display = 'inline'
+  setTimeout(() => {
+    copyMessage.style.display = 'none'
+  }, 2000)
+}
 
 const getParameter = (key) => {
   // Address of the current window
@@ -82,6 +107,10 @@ const getParameter = (key) => {
   // with the provided key
   return parameterList.get(key)
 }
+
+/* ==========================================================================
+DOM CONTENT LOADED
+========================================================================== */
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('script loaded')
@@ -118,17 +147,25 @@ document.addEventListener('DOMContentLoaded', () => {
               console.log(data)
               data.forEach((gif) => {
                 const collectionId = getParameter('collection')
+                //masonry-grid-item
                 const gridItem = document.createElement('div')
                 gridItem.classList.add('masonry-grid-item')
-                const gridLink = document.createElement('a')
-                gridLink.classList.add('masonry-grid-link')
-                gridLink.href = `index.php?route=gif&gif=${gif.id}`
+                //masonry span for gif modale
+                const spanGIF = document.createElement('span')
+                spanGIF.classList.add('masonry-grid-link', 'js-gif-modale')
+                spanGIF.id = `js-gif-id-${gif.id}`
+                spanGIF.position = 'relative'
+                //masonry gif img
                 const gifImg = document.createElement('img')
                 gifImg.classList.add('masonry-grid-img')
                 gifImg.alt = ''
                 gifImg.src = gif.link
-                gridLink.appendChild(gifImg)
-                gridItem.appendChild(gridLink)
+                gifImg.id = `js-gif-id-${gif.id}`
+
+                //appending all elements
+                spanGIF.appendChild(gifImg)
+                gridItem.appendChild(spanGIF)
+
                 if (
                   searchResultDisplay.classList.contains('js-collection-add')
                 ) {
@@ -141,20 +178,172 @@ document.addEventListener('DOMContentLoaded', () => {
                   )
                   gridAddLink.href = `index.php?route=add-gif-to-collection&gif=${gif.id}&collection=${collectionId}`
                   gridAddLink.textContent = 'Add'
-                  gridItem.appendChild(gridAddLink)
+                  spanGIF.appendChild(gridAddLink)
                 }
                 searchResultDisplay.appendChild(gridItem)
-              })
-              if (gifImg.complete) {
-                //load masonry display
-                var elem = document.querySelector('.masonry-grid')
-                var msnry = new Masonry(elem, {
-                  // options
-                  itemSelector: '.masonry-grid-item',
+                if (gifImg.complete) {
+                  //load masonry display
+                  var elem = document.querySelector('.masonry-grid')
+                  var msnry = new Masonry(elem, {
+                    // options
+                    itemSelector: '.masonry-grid-item',
 
-                  gutter: 2,
+                    gutter: 2,
+                  })
+                }
+                // Add event listener on each gif item
+                const gifModale = document.querySelector('.gif-display-modale')
+                const gifItems = document.querySelectorAll('.js-gif-modale')
+                const authOptions = document.querySelector(
+                  '.gif-card-auth-options'
+                )
+                gridItem.addEventListener('click', (e) => {
+                  console.log('event click modale')
+                  // If click on GIF
+
+                  //retrieve GIF id
+                  const gifIdName = e.target.id
+                  const gifId = gifIdName.slice(10)
+                  console.log(gifId)
+
+                  // generate report button
+                  authOptions.innerHTML = ''
+                  authOptions.classList.toggle('modale-hidden')
+                  const reportBtn = document.createElement('button')
+                  reportBtn.classList.add(
+                    'pink-button',
+                    'very-small-button',
+                    'gif-report-btn'
+                  )
+                  reportBtn.textContent = 'Report'
+                  reportBtn.addEventListener('click', () => {
+                    fetch(api + 'put-gif-reported&gif=' + gifId).then(
+                      (response) => {
+                        console.log(response)
+                        if (response.ok) {
+                          //response.json().then(console.log)
+                          response.json().then((data) => {
+                            console.log('La requête a  réussi')
+                            console.log(data)
+                            return data
+                          })
+                        } else {
+                          // La requete a echoué
+                          console.log('La requête a échoué')
+                          console.log(response)
+                        }
+                      }
+                    )
+                  })
+
+                  authOptions.appendChild(reportBtn)
+
+                  //Generate select
+                  const selectCollection = document.createElement('select')
+                  //fetch collections from user
+                  //for each collection generate an option with value = collection_id and collection name
+
+                  // Fetch gif infos from API
+                  fetch(api + 'get-gif-info&gif=' + gifId).then((response) => {
+                    console.log(response)
+                    if (response.ok) {
+                      //response.json().then(console.log)
+                      response.json().then((data) => {
+                        console.log('La requête a  réussi')
+                        console.log(data)
+                        // use fetched data to generate modale
+
+                        // add gif img
+                        const gifImg =
+                          document.querySelector('.js-gif-modale-img')
+                        gifImg.src = data[0].link
+                        const gifHashtags = document.querySelector(
+                          '.js-gif-modale-hashtags'
+                        )
+                        // add each hashtag link
+                        data.forEach((hashtag, i) => {
+                          if (i == 0) {
+                            gifHashtags.innerHTML = ''
+                          } else {
+                            const gifHashtag = document.createElement('a')
+                            gifHashtag.textContent = `#${hashtag.name}`
+                            gifHashtag.classList.add('gif-card-hashtag')
+                            gifHashtag.href = `index.php?route=hashtag-page&hashtag=${hashtag.id}`
+                            gifHashtags.appendChild(gifHashtag)
+                          }
+                        })
+                        // fill input with link to gif page
+                        const input = document.querySelector(
+                          '.js-gif-modale-input'
+                        )
+                        input.value = data[0].link
+                        //show modale
+                        gifModaleOverlay.classList.toggle('modale-hidden')
+                        gifModale.classList.toggle('modale-hidden')
+
+                        // Getting copy link button for gif modale
+                        const copyBtnGif =
+                          document.querySelector('.js-copy-btn-gif')
+                        if (copyBtnGif) {
+                          console.log('add copy adress event for gif')
+                          copyBtnGif.addEventListener('click', copyAdressGif)
+                        }
+
+                        //getting download button for gif modale
+                        const downloadBtn =
+                          document.querySelector('.js-download-gif')
+                        if (downloadBtn) {
+                          console.log('add event listener to dwnld btn')
+                          downloadBtn.addEventListener(
+                            'click',
+                            downloadGif,
+                            false
+                          )
+
+                          async function downloadGif() {
+                            console.log('enter download gif')
+                            //create new a element
+                            let a = document.createElement('a')
+                            // get image as blob
+                            let response = await fetch(data[0].link)
+                            let file = await response.blob()
+                            console.log(file)
+                            // use download attribute https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Attributes
+                            a.download = data[0].id
+                            a.href = window.URL.createObjectURL(file)
+                            console.log(a.href)
+                            //store download url in javascript https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes#JavaScript_access
+                            a.dataset.downloadurl = [
+                              'application/octet-stream',
+                              a.download,
+                              a.href,
+                            ].join(':')
+                            //click on element to start download
+                            a.click()
+                            window.URL.revokeObjectURL(file)
+                            downloadBtn.removeEventListener(
+                              'click',
+                              downloadGif,
+                              false
+                            )
+                            console.log('remove event listener to dwnld btn')
+                          }
+                        }
+                      })
+                    } else {
+                      // La requete a echoué
+                      console.log('La requête a échoué')
+                      console.log(response)
+                    }
+                  })
                 })
-              }
+                //Add keyboard control to open modale (accessibility)
+                gridItem.addEventListener('keydown', (event) => {
+                  if (event.code === 'Space' || event.code === 'Enter') {
+                    gifItem.click()
+                  }
+                })
+              })
             })
           } else {
             // La requete a echoué
@@ -172,6 +361,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const gifModale = document.querySelector('.gif-display-modale')
     const gifItems = document.querySelectorAll('.js-gif-modale')
     console.log('query gifItems')
+
+    // close modale on click on overlaw
+    window.onclick = function (event) {
+      if (event.target.classList.contains('gif-display-modale-overlay')) {
+        gifModale.classList.toggle('modale-hidden')
+        gifModaleOverlay.classList.toggle('modale-hidden')
+        authOptions.classList.toggle('modale-hidden')
+      }
+    }
 
     // Add event listener on each gif item
     gifItems.forEach((gifItem) => {
@@ -224,6 +422,37 @@ document.addEventListener('DOMContentLoaded', () => {
               if (copyBtnGif) {
                 console.log('add copy adress event for gif')
                 copyBtnGif.addEventListener('click', copyAdressGif)
+              }
+              //getting download button for gif modale
+              const downloadBtn = document.querySelector('.js-download-gif')
+              if (downloadBtn) {
+                console.log('add event listener to dwnld btn')
+                downloadBtn.addEventListener('click', downloadGif, false)
+
+                async function downloadGif() {
+                  console.log('enter download gif')
+                  //create new a element
+                  let a = document.createElement('a')
+                  // get image as blob
+                  let response = await fetch(data[0].link)
+                  let file = await response.blob()
+                  console.log(file)
+                  // use download attribute https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Attributes
+                  a.download = data[0].id
+                  a.href = window.URL.createObjectURL(file)
+                  console.log(a.href)
+                  //store download url in javascript https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes#JavaScript_access
+                  a.dataset.downloadurl = [
+                    'application/octet-stream',
+                    a.download,
+                    a.href,
+                  ].join(':')
+                  //click on element to start download
+                  a.click()
+                  window.URL.revokeObjectURL(file)
+                  downloadBtn.removeEventListener('click', downloadGif, false)
+                  console.log('remove event listener to dwnld btn')
+                }
               }
             })
           } else {
@@ -300,11 +529,11 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  if (searchResultDisplay) {
-    $container.imagesLoaded(function () {
-      $container.masonry()
-    })
-  }
+  // if (searchResultDisplay) {
+  //   $container.imagesLoaded(function () {
+  //     $container.masonry()
+  //   })
+  // }
   // //load masonry display
   // var elem = document.querySelector('.masonry-grid')
   // var msnry = new Masonry(elem, {
@@ -315,16 +544,16 @@ document.addEventListener('DOMContentLoaded', () => {
   // })
 })
 
-// window.onload = () => {
-//   // const masonryGrid = document.querySelector('.masonry-grid')
-//   // console.log(masonryGrid)
-//   // const masonry = new Masonry(grid)
+window.onload = () => {
+  // const masonryGrid = document.querySelector('.masonry-grid')
+  // console.log(masonryGrid)
+  // const masonry = new Masonry(grid)
 
-//   var elem = document.querySelector('.masonry-grid')
-//   var msnry = new Masonry(elem, {
-//     // options
-//     itemSelector: '.masonry-grid-item',
+  var elem = document.querySelector('.masonry-grid')
+  var msnry = new Masonry(elem, {
+    // options
+    itemSelector: '.masonry-grid-item',
 
-//     gutter: 5,
-//   })
-// }
+    gutter: 5,
+  })
+}
